8e2e6773b008dbe7e2df6f77d029817c34adedf8
jmizzoni <jpmizzoni@gmail.com>
2016-06-11 22:25:28 -0400

    install.sh installs vundle

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 cp .bash_aliases .bash_functions .bashrc .gitconfig .vimrc .tmux.conf ~/
 
+if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
 if [ ! -d ~/.vim/colors/ ]; then
     mkdir -p ~/.vim/colors
 fi
